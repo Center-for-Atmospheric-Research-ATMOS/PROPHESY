fb65f0af88d490cb8d5edef5695ec2f185c40fea
matthew <mozon21@univ.yo.oulu.fi>
2022-03-31 16:14:25 +0300

    simple acquisition simulation: only the geometry factor is accounted for

--- test/finger_and_planar_model.jl
+++ test/finger_and_planar_model.jl
@@ -1,24 +1,24 @@
 
 H_z_far,H_zxy_far,_,_,_   = plane_gain_H(x_far,Y,z_surf,x0_far,y0_far,z0_far,λe);
 H_z_near,H_zxy_near,_,_,_ = plane_gain_H(x_near,Y,z_surf,x0_near,y0_near,z0_near,λe);
-H_z_finger_far,_          = finger_gain_H(0.0,0.0,z_surf,0.0,0.0,z0_far,λe);
-H_z_finger_near,_         = finger_gain_H(0.0,0.0,z_surf,0.0,0.0,z0_near,λe);
+H_z_finger_far_fp,_          = finger_gain_H(0.0,0.0,z_surf,0.0,0.0,z0_far,λe);
+H_z_finger_near_fp,_         = finger_gain_H(0.0,0.0,z_surf,0.0,0.0,z0_near,λe);
 # H_z_z_0,_                 = finger_gain_H(0.0,0.0,z_surf,0.0,0.0,z0_far,0.58λe);
 
-relative_eal_far  = log.(H_z_finger_far[2:end-1]/H_z_finger_far[end-1])./log.(H_z_far[2:end-1]/H_z_far[end-1]);
-relative_eal_near = log.(H_z_finger_near[2:end-1]/H_z_finger_near[end-1])./log.(H_z_near[2:end-1]/H_z_near[end-1]);
-r_eal_min_far,r_eal_max_far   = extrema(relative_eal_far[1:end-1])
-r_eal_min_near,r_eal_max_near = extrema(relative_eal_near[1:end-1])
+relative_eal_far_fp  = log.(H_z_finger_far_fp[2:end-1]/H_z_finger_far_fp[end-1])./log.(H_z_far[2:end-1]/H_z_far[end-1]);
+relative_eal_near_fp = log.(H_z_finger_near_fp[2:end-1]/H_z_finger_near_fp[end-1])./log.(H_z_near[2:end-1]/H_z_near[end-1]);
+r_eal_min_far_fp,r_eal_max_far_fp   = extrema(relative_eal_far_fp[1:end-1])
+r_eal_min_near_fp,r_eal_max_near_fp = extrema(relative_eal_near_fp[1:end-1])
 
-H_z_far_min,_          = finger_gain_H(x0_far,y0_far,z_surf,x0_far,y0_far,z0_far,r_eal_min_far*λe);
-H_z_far_max,_          = finger_gain_H(x0_far,y0_far,z_surf,x0_far,y0_far,z0_far,r_eal_max_far*λe);
-H_z_near_min,_         = finger_gain_H(x0_near,y0_near,z_surf,x0_near,y0_near,z0_near,r_eal_min_near*λe);
-H_z_near_max,_         = finger_gain_H(x0_near,y0_near,z_surf,x0_near,y0_near,z0_near,r_eal_max_near*λe);
+H_z_far_min_fp,_          = finger_gain_H(x0_far,y0_far,z_surf,x0_far,y0_far,z0_far,r_eal_min_far_fp*λe);
+H_z_far_max_fp,_          = finger_gain_H(x0_far,y0_far,z_surf,x0_far,y0_far,z0_far,r_eal_max_far_fp*λe);
+H_z_near_min_fp,_         = finger_gain_H(x0_near,y0_near,z_surf,x0_near,y0_near,z0_near,r_eal_min_near_fp*λe);
+H_z_near_max_fp,_         = finger_gain_H(x0_near,y0_near,z_surf,x0_near,y0_near,z0_near,r_eal_max_near_fp*λe);
 
 
 figure();
-plot(z_surf[2:end-1],relative_eal_far, label="far")
-plot(z_surf[2:end-1],relative_eal_near, label="near")
+plot(z_surf[2:end-1],relative_eal_far_fp, label="far")
+plot(z_surf[2:end-1],relative_eal_near_fp, label="near")
 legend()
 xlabel("depth [\$\\mu m\$]")
 ylabel("log ratio: \$\\frac{\\log(\\frac{I^{\\mathrm{planar}}}{I^{\\mathrm{planar}}_0} )}{\\log(\\frac{I^{\\mathrm{cylindrical}}}{I^{\\mathrm{cylindrical}}_0})}\$")
@@ -27,12 +27,12 @@ ylabel("log ratio: \$\\frac{\\log(\\frac{I^{\\mathrm{planar}}}{I^{\\mathrm{plana
 figure();
 plot(z_surf,H_z_far/maximum(H_z_far), label="planar: far \$\\lambda_e\$", color="tab:blue");
 plot(z_surf,H_z_near/maximum(H_z_near), label="planar: near \$\\lambda_e\$", color="tab:green");
-plot(z_surf,H_z_finger_far/maximum(H_z_finger_far), label="finger approximation: far \$\\lambda_e\$", color="tab:orange");
-plot(z_surf,H_z_finger_near/maximum(H_z_finger_near), label="finger approximation: near \$\\lambda_e\$", color="tab:olive");
+plot(z_surf,H_z_finger_far_fp/maximum(H_z_finger_far_fp), label="finger approximation: far \$\\lambda_e\$", color="tab:orange");
+plot(z_surf,H_z_finger_near_fp/maximum(H_z_finger_near_fp), label="finger approximation: near \$\\lambda_e\$", color="tab:olive");
 s = @sprintf "finger approximation limits (far)" #  \$[%.2f\\lambda_e,%.2f\\lambda_e]\$" r_eal_min_far r_eal_max_far
-fill_between(z_surf,H_z_far_min/maximum(H_z_far_min),H_z_far_max/maximum(H_z_far_max),alpha=0.5,color="tab:red",label=s)
+fill_between(z_surf,H_z_far_min_fp/maximum(H_z_far_min_fp),H_z_far_max_fp/maximum(H_z_far_max_fp),alpha=0.5,color="tab:red",label=s)
 s = @sprintf "finger approximation limits (near)" #  \$[%.2f\\lambda_e,%.2f\\lambda_e]\$" r_eal_min_near r_eal_max_near
-fill_between(z_surf,H_z_near_min/maximum(H_z_near_min),H_z_near_max/maximum(H_z_near_max),alpha=0.5,color="tab:pink",label=s)
+fill_between(z_surf,H_z_near_min_fp/maximum(H_z_near_min_fp),H_z_near_max_fp/maximum(H_z_near_max_fp),alpha=0.5,color="tab:pink",label=s)
 legend()
 xlabel("depth [\$\\mu m\$]")
 ylabel("normalized gain")
--- test/geom.jl
+++ test/geom.jl
@@ -6,21 +6,17 @@ fm.findfont("serif", rebuild_if_missing=false)
 fm.findfont("serif", fontext="afm", rebuild_if_missing=false)
 rc("font",family="serif",serif="Computer Modern Roman")
 rc("text", usetex=true)
-using myPlot
 
 # data manipulation (loading, writing, etc)
-using CSV
-using Dates
-using DataFrames # for loading and saving the results, e.g. CSV.write("measured_psd.csv",DataFrame(PSD_meas'); writeheader=false)
 using Printf
 
 # scientific package from the official Julia repositories
-using LinearAlgebra
-using Statistics
-using DSP
-using SpecialMatrices
-using Polynomials
-using StatsBase
+# using LinearAlgebra
+# using Statistics
+# using DSP
+# using SpecialMatrices
+# using Polynomials
+# using StatsBase
 
 # implemented scientific packages
 using utilsFun  # for the softMax functions
@@ -95,46 +91,14 @@ include("finger_and_planar_model.jl")
 
 
 
+##
+## acquisition simulation
+##
 
-if false
 # simulate some data (one point in the kinetic energy spectrum for four different concentration profiles)
 ρA_1 = logistic.(1000.0reverse(μ0.-r_surf).-2.0,0.0,1.0,2.0);
 ρA_2 = logistic.(1000.0reverse(μ0.-r_surf).-2.0,0.0,1.0,2.0) .+ 2.0exp.(-(1000.0reverse(μ0.-r_surf).-1.0).^2. /(2.0*0.25^2));
 ρA_3 = logistic.(1000.0reverse(μ0.-r_surf).-2.0,0.0,1.0,2.0) .+ exp.(-(1000.0reverse(μ0.-r_surf).-1.5).^2. /(2.0*0.5^2));
 ρA_4 = exp.(-(1000.0reverse(μ0.-r_surf).-2.5).^2. /(2.0*0.5^2));
 
-# for each cases (cylindrical near and far, and planar), simulate the acquisition of one datum for each profile
-M_far  = [H_r_far'*reverse(ρA_1); H_r_far'*reverse(ρA_2); H_r_far'*reverse(ρA_3); H_r_far'*reverse(ρA_4)];
-M_near = [H_r_near'*reverse(ρA_1); H_r_near'*reverse(ρA_2); H_r_near'*reverse(ρA_3); H_r_near'*reverse(ρA_4)];
-M_z    = [H_z'*reverse(ρA_1); H_z'*reverse(ρA_2); H_z'*reverse(ρA_3); H_z'*reverse(ρA_4)];
-
-## plot
-figure(figsize=[10,5])
-ax1 = subplot(121)
-plot(r_surf,H_r_far/maximum(H_r_far), label="cylinder: far \$\\lambda_e\$", color="tab:blue");
-plot(r_surf,H_r_near/maximum(H_r_near), label="cylinder: near \$\\lambda_e\$", color="tab:green");
-plot(r_surf,H_z/maximum(H_z), label="planar approximation \$\\lambda_e\$", color="tab:orange");
-s = @sprintf "planar approximation limits (far)" #  \$[%.2f\\lambda_e,%.2f\\lambda_e]\$" r_eal_min_far r_eal_max_far
-fill_between(r_surf,H_z_far_min/maximum(H_z_far_min),H_z_far_max/maximum(H_z_far_max),alpha=0.5,color="tab:red",label=s)
-s = @sprintf "planar approximation limits (near)" #  \$[%.2f\\lambda_e,%.2f\\lambda_e]\$" r_eal_min_near r_eal_max_near
-fill_between(r_surf,H_z_near_min/maximum(H_z_near_min),H_z_near_max/maximum(H_z_near_max),alpha=0.5,color="tab:pink",label=s)
-legend()
-xlabel("radius [\$\\mu m\$]")
-ylabel("normalized gain")
-
-ax2 = subplot(122)
-scatter(["\$\\rho_1\$"; "\$\\rho_2\$"; "\$\\rho_3\$"; "\$\\rho_4\$"],M_far./M_z, label="cylinder far vs planar", color="tab:blue")
-scatter(["\$\\rho_1\$"; "\$\\rho_2\$"; "\$\\rho_3\$"; "\$\\rho_4\$"],M_near./M_z, label="cylinder near vs planar", color="tab:green")
-xlabel("concentration profiles")
-ylabel("relative acquisition: \$\\frac{I_{\\mathrm{cylinder}}}{I_{\\mathrm{plane}}}\$")
-ylim(0.0)
-legend()
-
-tight_layout(pad=1.0, w_pad=2.0, h_pad=0.2)
-ax2.annotate("a)", xy=(3, 1),  xycoords="data", xytext=(-1.27, 0.99), textcoords="axes fraction", color="black",fontsize=14)
-ax2.annotate("b)", xy=(3, 1),  xycoords="data", xytext=(-0.07, 0.99), textcoords="axes fraction", color="black",fontsize=14)
-
-# savefig("plans_vs_cylinder.png")
-# savefig("plans_vs_cylinder.pdf")
-
-end
+include("geom_acquisition.jl")
