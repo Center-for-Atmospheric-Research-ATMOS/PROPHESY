55e3beb36ea49cca14b156d17b3359beea678117
matthew <matthew.ozon@gmail.com>
2022-04-25 17:09:51 +0300

    truncated model with and without uncertainty: implemented and tested

--- test/testTruncation.jl
+++ test/testTruncation.jl
@@ -59,12 +59,18 @@ D_2nd_lowres = D2nd(N_lowres);
 
 
 H0 = H_lowres[:,1];
+μH0 = μH'[:,1];
 H_tilde = H_lowres[:,2:N0_lowres];
+μH_tilde = μH'[:,2:N0_lowres];
 Hb = H_lowres[:,N0_lowres+1:end];
+μHb = μH'[:,N0_lowres+1:end];
 Δy = dropdims(sum(Hb,dims=2),dims=2)*ρA_1[end] # Hb*ρA_1[N0+1:end];
 δy = H0*ρA_1[1];
+Δyμ = dropdims(sum(μHb,dims=2),dims=2)*ρA_1[end] # Hb*ρA_1[N0+1:end];
+δyμ = μH0*ρA_1[1];
 
-y_tilde = y_data.-(Δy+δy)';
+y_tilde  = y_data.-(Δy+δy)';
+y_tildeμ = y_data.-(Δyμ+δyμ)';
 
 DN = D2nd(N_lowres+3); #+3
 D0 = DN[:,1];
@@ -77,6 +83,19 @@ Db = DN[:,N_lowres+2:N_lowres+3];
 yd = Δyd+δyd;
 
 Htrunc = [H_tilde; D_tilde];
+
+Htrunc_un = [μH_tilde; D_tilde; diagm(ones(Cdouble,N_lowres))]
+
+ΓH0b = Array{Cdouble,3}(undef,Nr_lowres-N_lowres,Nr_lowres-N_lowres,Ndata);
+for i in 1:Ndata
+    ΓH0b[1,1,i] = ΓH[1,1,i]
+    ΓH0b[1,2:end,i] = ΓH[1,N0_lowres+1:end,i]
+    ΓH0b[2:end,1,i] = ΓH[N0_lowres+1:end,1,i]
+    ΓH0b[2:end,2:end,i] = ΓH[N0_lowres+1:end,N0_lowres+1:end,i]
+end
+σεH = sqrt.(dropdims(sum(ΓH0b,dims=(1,2)),dims=(1,2)));
+σB = 0.01 # 0.05;
+
 
 # covariance matrix for the a priori distribution
 
@@ -100,9 +119,8 @@ for i in 1:Nr_lowres
         Γprior_lowres[j,i] = Γprior_lowres[i,j]
     end
 end
-# Γytilde = ΓI[:,:,1] + 1.0e-6*(Γprior[1,1]*H0*H0' + Hb*Γprior[N+2:end,N+2:end]*Hb');
-Γd = 0.0001*Γprior[2:N+2,2:N+2]; # inv(D_tilde'*inv(Γprior[2:N+1,2:N+1])*D_tilde)
-Γd_lowres = 0.0001*((Nr/Nr_lowres)^2)*Γprior_lowres[2:N_lowres+2,2:N_lowres+2];
+Γd = (0.01)^2*Γprior[2:N+2,2:N+2]; 
+Γd_lowres = (0.01)^2*((Nr/Nr_lowres)^2)*Γprior_lowres[2:N_lowres+2,2:N_lowres+2];
 
 
 # 
@@ -118,8 +136,10 @@ r_y_tol=0.005;
 # for each noise sample
 # ρ_est_block    = zeros(Cdouble,Nnoise,Nr);
 # ρ_est_cp_block = zeros(Cdouble,Nnoise,Nr);
-ρ_est_block    = zeros(Cdouble,Nnoise,Nr_lowres);
-ρ_est_cp_block = zeros(Cdouble,Nnoise,Nr_lowres);
+ρ_est_block       = zeros(Cdouble,Nnoise,Nr_lowres);
+ρ_est_cp_block    = zeros(Cdouble,Nnoise,Nr_lowres);
+ρ_est_cp_block_un = zeros(Cdouble,Nnoise,Nr_lowres);
+ΓHΓyinv           = zeros(Cdouble,N_lowres,N_lowres);
 
 
 
@@ -136,14 +156,26 @@ for i in 1:Nnoise
     # local ρ_est,sn,taun,X_ALL,S_ALL,T_ALL,N_last = alg2_cp_gaussian(0.5ones(Cdouble,N),Y,Htrunc,w,W_stop;tau0=τ0,Niter=N_max_iter,r_n_tol=r_n_tol,r_y_tol=r_y_tol);
     # local ρ_est,sn,taun,X_ALL,S_ALL,T_ALL,N_last = alg2_cp_quad(0.5ones(Cdouble,N),y_tilde[i,:],yd,Htrunc,ΓI[:,:,i],Γd::Array{Cdouble,2},W_stop;τ0=τ0,Niter=N_max_iter,r_n_tol=r_n_tol,r_y_tol=r_y_tol)
     local ρ_est,sn,taun,X_ALL,S_ALL,T_ALL,N_last = alg2_cp_quad(0.5ones(Cdouble,N_lowres),y_tilde[i,:],yd,Htrunc,ΓI[:,:,i],Γd_lowres,W_stop_lowres;τ0=τ0,Niter=N_max_iter,r_n_tol=r_n_tol,r_y_tol=r_y_tol)
-
+    
+    # marginalization of uncertainty
+    global ΓHΓyinv .= 0.0;
+    for k in 1:Ndata
+        # ΓHΓyinv = ΓHΓyinv + diagm(diag(ΓH[2:N0_lowres,2:N0_lowres,k])/ΓI[k,k,i])
+        ΓHΓyinv = ΓHΓyinv + ΓH[2:N0_lowres,2:N0_lowres,k]/(ΓI[k,k,i]+(σB*σεH[k])^2)
+    end
+    local ρ_est_un,sn,taun,X_ALL,S_ALL,T_ALL,N_last = alg2_cp_quad_un(0.5ones(Cdouble,N_lowres),y_tildeμ[i,:],yd,Htrunc_un,ΓI[:,:,i],Γd_lowres,ΓHΓyinv,W_stop_lowres;τ0=τ0,Niter=N_max_iter,r_n_tol=r_n_tol,r_y_tol=r_y_tol)
+    
     # ρ_est_cp_block[i,:] = [ρA_1[1]; ρ_est; ρA_1[N_lowres+2:end]]
     ρ_est_cp_block[i,:] = [ρA_1[1]; ρ_est; ρA_1[end]*ones(Cdouble,Nr_lowres-N0_lowres)]
+    ρ_est_cp_block_un[i,:] = [ρA_1[1]; ρ_est_un; ρA_1[end]*ones(Cdouble,Nr_lowres-N0_lowres)]
 end
 
 
 μρ = dropdims(mean(ρ_est_cp_block,dims=1),dims=1);
 Γρ = cov(ρ_est_cp_block);
+
+μρ_un = dropdims(mean(ρ_est_cp_block_un,dims=1),dims=1);
+Γρ_un = cov(ρ_est_cp_block_un);
 
 figure()
 plot(μ0.-r_lowres,ρ_est_block')
@@ -154,10 +186,17 @@ figure()
 plot(μ0.-r_lowres,ρ_est_cp_block')
 plot(μ0.-r,ρA_1)
 
+figure()
+# plot(ρ_est_block')
+plot(μ0.-r_lowres,ρ_est_cp_block_un')
+plot(μ0.-r,ρA_1)
+
 
 figure()
 plot(1000.0(r_lowres.-μ0),reverse(μρ),color="tab:blue",label="mean value")
 fill_between(1000.0(r_lowres.-μ0),reverse(μρ-sqrt.(diag(Γρ))),reverse(μρ+sqrt.(diag(Γρ))),alpha=0.5,color="tab:blue",label="uncertainty")
+plot(1000.0(r_lowres.-μ0),reverse(μρ_un),color="tab:orange",label="mean value marginal")
+fill_between(1000.0(r_lowres.-μ0),reverse(μρ_un-sqrt.(diag(Γρ_un))),reverse(μρ_un+sqrt.(diag(Γρ_un))),alpha=0.5,color="tab:orange",label="uncertainty marginal")
 plot(1000.0(r.-μ0),reverse(ρA_1),color="tab:green",label="GT")
 legend(fontsize=14)
 xlabel("depth [nm]",fontsize=14)
