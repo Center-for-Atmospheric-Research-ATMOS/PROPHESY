f940be7764a5702dc9acf895e5e6c20c40b519d9
matthew <matthew.ozon@gmail.com>
2022-08-19 01:27:23 +0300

    adding O1s photoionization cross section values

--- src/XPSpack/cross_section.jl
+++ src/XPSpack/cross_section.jl
@@ -1,10 +1,20 @@
 # load the interpolation points from file
 include("C1s.jl")
 σ_C1s_interp = extrapolate(interpolate((σ_C1s[:,1],), σ_C1s[:,2], Gridded(Linear())),Line())
-function σ_cs_orb(ħν::Cdouble,nl::String="C1s") # for the time being, only implement C1s, but you can get other orbitales from https://vuo.elettra.eu/services/elements/WebElements.html
-   σ_C1s_interp(ħν)
-end
 
+include("O1s.jl")
+σ_O1s_interp = extrapolate(interpolate((σ_O1s[:,1],), σ_O1s[:,2], Gridded(Linear())),Line())
+
+
+function σ_cs_orb(ħν::Cdouble,nl::String="C1s") # for the time being, only implement C1s, but you can get other orbitales from https://vuo.elettra.eu/services/elements/WebElements.html
+   val = 0.0
+   if (nl=="C1s")
+      val = σ_C1s_interp(ħν)
+   else
+      val = σ_O1s_interp(ħν)
+   end
+   val
+end
 
 """
    σ_bg(Ke::Cdouble)
