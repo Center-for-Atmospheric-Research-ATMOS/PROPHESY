7dbc7c0d9d479fedc1da3674c8fdd7f97070d796
matthew <matthew.ozon@gmail.com>
2022-08-25 16:56:29 +0300

    little helper for loading data

---
+++ src/XPSfile.jl
@@ -0,0 +1,25 @@
+# push!(LOAD_PATH,"/home/mattoz/Dev/XPSinv.jl/src/")
+# sudo nano /opt/julias/julia-1.3.1/etc/julia/startup.jl
+
+"""
+
+    little helper: loading data from files such as xlsx and formatting them into DataFrame and Dictionary
+
+"""
+module XPSfile
+
+using XLSX
+using DataFrames
+using Query
+using DataValues
+
+export XLSX,DataFrames,Query,DataValues
+
+# importing and formatting data from xlsx files to dtaframes/dictionaries
+export dataAndFit_xlsx2df,curveFromFit
+
+export dataAndMeta_xlsx2df, model_xlsx2df
+
+
+include("XPSfile/XPSxlsx.jl")
+end
