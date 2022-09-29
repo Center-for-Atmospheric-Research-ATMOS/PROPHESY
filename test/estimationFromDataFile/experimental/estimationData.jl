41fccf878a3b67a8921249eeee31d89a28b25740
matthew <matthew.ozon@gmail.com>
2022-09-30 00:04:25 +0300

    profile reconstruction: simulated and experimental

---
+++ test/estimationFromDataFile/experimental/estimationData.jl
@@ -0,0 +1,135 @@
+# load simulated data, and fit the peaks, estimate the alignment factor, remove the noise and estimate the sensitivity matrices
+## load the packages used in the estimation
+# plotting
+using PyPlot
+rc("text", usetex=true)
+rc("figure",max_open_warning=50)
+# using myPlot
+color_array = ["tab:blue"; "tab:orange"; "tab:green"; "tab:red"; "tab:purple"; "tab:brown"; "tab:pink"; "tab:gray"; "tab:olive"; "tab:cyan"; "magenta"; "yellow"; "hotpink"; "darkmagenta"; "chartreuse"; "deepskyblue"; "navy"; "darkcyan"; "crimson"; "firebrick"]; 
+
+# data manipulation (loading, writing, etc)
+using Printf
+using XPSfile
+using DataFrames
+using Query
+
+# scientific package from the official Julia repositories
+using LinearAlgebra
+using StatsBase
+
+
+# modeling XPS
+using XPSpack # experiment model (geometry factor and cross section estimation)
+
+# inversion package
+using XPSinv
+
+SAMPLING = true
+if SAMPLING
+    using XPSsampling
+end
+
+PLOT_FIG  = true
+PLOT_DATA = false
+
+# folders where the data files are
+data_folder = "../../../data/TK/";
+data_folderC1s = string(data_folder,"C1s/")
+
+# flags
+FLAG_PLOT = true;
+FLAG_SAVE_PLOT = false;
+
+# geometry setup
+λe0 = 2.0e-3;         # reference penetration depth in μm
+δr = 2.0e-3           # transition to vacuum layer thickness (let's set about 1 nm)
+k0 = 10;              # compute the measurement model over a distance of k0*λe0
+Nr = 101; # 201;      # number of discretization points in the radial dimension
+Nθ = 256;             # number of discretization points in the polar angle dimension
+Ny = 256;             # number of discretization points in the cylinder axis dimension
+L  = 50.0;            # height of the irradiated sample (the vertical extent of the beam is more like 20μm instead of 100μm)
+μ0 = 10.0;            # radius of the cylinder
+x0 = sqrt(2.0)*5000.0 # (x0,y0,z0) are the coordinates of the analyzer's apperture
+y0 = 0.0;
+z0 = 5000.0;
+
+# spacial discretization 
+r = collect(range(μ0-k0*λe0,μ0+δr,length=Nr));
+θ0 = atan(x0,z0)
+θ = collect(range(θ0-π/2.0,θ0+π/2.0,Nθ));
+y = collect(range(-L/2.0,L/2.0,length=Ny));
+
+# unit conversion constant (some of the quantities are in μm, some in L and some in Mbarn)
+NA = 6.022e23;
+κ_simple_units = 1.0e-37*NA; # simplified model
+κ_units        = 1.0e-25*NA; # original model
+
+
+
+# column names fo the fitted results
+photon_sym = Symbol("Photon energy");
+ph_flu_sym = Symbol("Photon flux");
+bind_sym   = Symbol("Binding energy");
+shift_sym  = Symbol("Peak shift");
+gauss_sym  = Symbol("FWHM(G)");
+loren_sym  = Symbol("FWHM(L)");
+area_sym   = Symbol("Area");
+σtot_sym   = Symbol("Sigma");
+α_al_sym   = Symbol("Alignment");
+
+# regular expression to navigate the files
+regData    = r"Eph ?= ?"                  # pattern found in the data sheet's name that does not appear in other sheets
+regFit     = r"[Ff]itt?ing(_| )?results?" # pattern found in fit sheet's name
+regEph     = r"=[0-9]*eV"                 # pattern for reading the photon energy
+sortBy     = Symbol("Photon energy")      # column name for the photon energy  (sort the data by increasing order of photon energy)
+thenSortBy = Symbol("Binding energy")     # colmun name for the binding energy (for the data sharing the same photon energy, sort them by binding energy)
+
+
+# alignement from C1s data
+include("loadDataAndAlignment.jl")
+
+# now, you can run the inversion with the data
+
+y_data_1 # SNR OK
+y_data_2 # SNR OK ish
+y_data_3 # SNR... well, there's no need to try this one
+σ_noise  # standard deviation of the normalized noise
+
+
+##
+## run the inversion using only y_data_1
+##
+include("profileReconstruction.jl")
+
+##
+## plot the results
+##
+if PLOT_FIG
+    figure()
+    l_ρ, = plot(1.0e3*(r.-μ0),ρC1s_bulk*ρ_cp,color=color_array[1])
+    if SAMPLING
+        l_ρ_std =  fill_between(1.0e3*(r[N0:end-1].-μ0),ρC1s_bulk*(ρ_est-stdρ_HI),ρC1s_bulk*(ρ_est+stdρ_HI),alpha=0.5,color=color_array[1])
+    end
+    xlim(1.0e3*(r[1].-μ0),2.5e3*(r[end].-μ0))
+    ylim(-0.01ρC1s_bulk,max(1.6ρC1s_bulk,1.1maximum(ρC1s_bulk*(ρ_est+stdρ_HI))))
+    xlabel("depth [nm]",fontsize=14); 
+    ylabel("molar concentraion [M]",fontsize=14) 
+    xticks(fontsize=14); yticks(fontsize=14); 
+    ax1 = gca()
+    ax1.ticklabel_format(axis="y",style="sci",scilimits=(-1,1),useOffset=true)
+    ax1.yaxis.offsetText.set_size(14)
+    ax1.xaxis.offsetText.set_size(14)
+    # legend(fontsize=12)
+    legend([(l_ρ,l_ρ_std)],["estimates\$\\pm\\sigma\$"],fontsize=12,borderpad=0.4,borderaxespad=0.2,handletextpad=0.5,handlelength=1.0,framealpha=0.4)
+    tight_layout(pad=1.0, w_pad=0.2, h_pad=0.2)
+    ax1.text(0.05, 0.1, replace(data_filesC1s[idx_file][1:end-5],"_"=>" "), transform=ax1.transAxes,fontsize=16)
+    
+    savefig(string(data_filesC1s[idx_file][1:end-5],"_reconstruction.png"))
+    savefig(string(data_filesC1s[idx_file][1:end-5],"_reconstruction.pdf"))
+
+    if SAMPLING
+        figure()
+        plot(deltaU)
+    end
+end
+
---
+++ test/estimationFromDataFile/experimental/loadDataAndAlignment.jl
@@ -0,0 +1,105 @@
+# find the data files
+folder_content = readdir(data_folderC1s);
+list_match = match.(r"xlsx$",folder_content)
+data_filesC1s = folder_content[list_match.!=nothing]
+
+# idx_file = 1; # not enough data
+idx_file = 2; # OK
+# idx_file = 3; # not so good, probably problem in the data
+# idx_file = 4; # OK
+
+
+fileName = data_filesC1s[idx_file][1:end-5];
+
+ρC1s_out  = 0.0                                                                  # [M] concentration of C 1s outside of the sharp edge volume (at the outter boundary)
+ρC1s_bulk = 1.0e-3parse(Cdouble,match(r"SDS_[0-9]*mM",fileName).match[5:end-2]); # [M] bulk concentration of C 1s
+
+dictAllData,df_fit,Ndata = dataAndFit_xlsx2df(string(data_folderC1s,fileName,".xlsx"); regData=regData, regFit=regFit, regEph=regEph, sortBy=sortBy, thenSortBy=thenSortBy);
+
+# list all the photon energies
+df_Eph = select(df_fit,photon_sym) |> @unique() |> @orderby(_[photon_sym]) |> DataFrame
+# put everything in nice boxes
+dictPeak = Dict();
+for i in df_Eph[!,photon_sym]
+    local df = df_fit |> @filter(_[photon_sym]==i) |> DataFrame
+    dictPeak[Symbol(string("hν_",i))] = df # (df,names(df))
+end
+
+
+# alignement
+α_al_noise = zeros(Cdouble,Ndata);
+α_al_ratio = zeros(Cdouble,Ndata);
+T          = zeros(Cdouble,Ndata);
+Fν         = zeros(Cdouble,Ndata);
+σ_C1s      = zeros(Cdouble,Ndata);
+H_geom     = zeros(Cdouble,Ndata,Nr);
+σ_data     = zeros(Cdouble,Ndata);
+y_peak_1   = zeros(Cdouble,Ndata);
+y_peak_2   = zeros(Cdouble,Ndata);
+y_peak_3   = zeros(Cdouble,Ndata);
+
+if FLAG_PLOT
+    figure(figsize=[12, 10])
+end
+for i in 1:Ndata
+    # get fitting data into arrays
+    local plot_sym = Symbol(string("hν_",df_Eph[!,photon_sym][i]));
+    local Be       = dictAllData[plot_sym].Wavelength;
+    local dKe      = median(abs.(Be[2:end]-Be[1:end-1]))
+    local Npeaks, BePeak, σePeak, σePeakL, AePeak, σ_peak = curveFromFit(dictPeak[plot_sym],Be,true;bind_sym=bind_sym, shift_sym=shift_sym, gauss_sym=gauss_sym, area_sym=area_sym, loren_sym=loren_sym);
+
+    # get peak areas
+    y_peak_1[i] = dKe*sum(dictAllData[plot_sym].Curve1); # dKe*AePeak[1]
+    y_peak_2[i] = dKe*sum(dictAllData[plot_sym].Curve2); # dKe*AePeak[2]
+    y_peak_3[i] = dKe*sum(dictAllData[plot_sym].Curve3); # dKe*AePeak[3]
+
+    # plot data and fits
+    if FLAG_PLOT
+        local ax = subplot(2,2,i)
+        plot(Be,dictAllData[plot_sym].Raw_spectrum,label="Data")
+        plot(Be,dictAllData[plot_sym].Background.+dKe*dropdims(AePeak'*σ_peak,dims=1),label="fitted spectra peaks")
+        plot(Be,dictAllData[plot_sym].Background.+dictAllData[plot_sym].Curve1.+dictAllData[plot_sym].Curve2.+dictAllData[plot_sym].Curve3,label="fitted spectra curves")
+        ylim(0.0)
+        xlabel("binding energy [eV]",fontsize=14); 
+        ylabel("spectrum [count]",fontsize=14) 
+        xticks(fontsize=14); yticks(fontsize=14); 
+        legend(fontsize=14)
+        ax.invert_xaxis()
+        ax.text(0.1, 0.5, string("Eph = ",df_Eph[!,photon_sym][i]," [eV]"), transform=ax.transAxes,fontsize=14)
+    end
+    
+    # measurement model
+    local λe = 1.0e-3dictPeak[plot_sym][!,:IMFP][2]
+    local σ_all = dictAllData[plot_sym].Curve1.+dictAllData[plot_sym].Curve2.+dictAllData[plot_sym].Curve3;
+    σ_all = σ_all/(dKe*sum(σ_all));
+    # compute the geomtry factor
+    H_geom[i,:],_,_,_,_ = cylinder_gain_H(r,θ,y,x0,y0,z0,μ0,λe);
+    # estimate the alignment parameter
+    local Sbg = dictAllData[plot_sym].Background;
+    local S_noisy = dictAllData[plot_sym].Raw_spectrum;
+    local ρ = ρC1s_bulk*ones(Cdouble,Nr)
+    α_al_noise[i],noise_data = noiseAndParameterEstimation(σ_all,H_geom[i,:],S_noisy,Sbg,ρ)
+    α_al_noise[i]            = α_al_noise[i]/(κ_units*σ_C1s_exp(convert(Cdouble,df_Eph[!,photon_sym][i]))*dictPeak[plot_sym][!,ph_flu_sym][1])
+    α_al_ratio[i]            = dictPeak[plot_sym][!,α_al_sym][1]
+    σ_data[i]                = sqrt.(var(noise_data))
+    Fν[i]                    = dictPeak[plot_sym][!,ph_flu_sym][1];
+    T[i]                     = 1.0;
+    σ_C1s[i]                 = σ_C1s_exp(convert(Cdouble,df_Eph[!,photon_sym][i]));
+end
+
+y_data_1 = y_peak_1./(α_al_noise.*T.*Fν.*σ_C1s.*ρC1s_bulk*κ_units) 
+y_data_2 = y_peak_2./(α_al_noise.*T.*Fν.*σ_C1s.*ρC1s_bulk*κ_units)
+y_data_3 = y_peak_3./(α_al_noise.*T.*Fν.*σ_C1s.*ρC1s_bulk*κ_units)
+σ_noise  = σ_data./(α_al_noise.*T.*Fν.*σ_C1s.*ρC1s_bulk*κ_units)
+
+SNR_1 = (y_data_1./σ_noise).^2;
+SNR_2 = (y_data_2./σ_noise).^2;
+SNR_3 = (y_data_3./σ_noise).^2;
+
+if FLAG_PLOT
+    tight_layout(pad=1.0, w_pad=0.5, h_pad=0.2)
+    if FLAG_SAVE_PLOT
+        savefig(string(fileName,"_plot.pdf"))
+        savefig(string(fileName,"_plot.png"))
+    end
+end
