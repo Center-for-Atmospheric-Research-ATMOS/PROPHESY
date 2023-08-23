4ba1e254eec494289f5117d27e4ece846ae29138
matthewozon <matthew.ozon@gmail.com>
2023-08-23 19:02:12 +0300

    adding smooth edge cases

--- packages/XPSpack/src/XPSalignment.jl
+++ packages/XPSpack/src/XPSalignment.jl
@@ -1,12 +1,4 @@
 
-#------------------------------------------------------------------------------
-#
-# This file is part of the XPSpack module which is licensed under CC-BY 4.0 license.
-#
-# Copyright (C) 2022,  Matthew Ozon, Konstantin Tumashevich and Nønne L. Prisle.
-#
-#------------------------------------------------------------------------------
-
 """
     beamProfile: structure describing the extent of the spread in space of the photon beam
     The beam density is taken to be a cross section of the parallel beam (orthogonal to the propagation direction)
@@ -47,8 +39,6 @@ end
     λe is the attenuation length in the Beer-Lambert model
     bp: a beam profile object that describes the deviation from the center of the target and the spatial spread of the beam
 
-    Note: does not include the profile of the beam light, 
-    but it can be add easily if known, see alignmentParameter
 """
 function alignmentParameter(bp::beamProfile,r::Array{Cdouble,1},θ::Array{Cdouble,1},y::Array{Cdouble,1},x0::Cdouble,y0::Cdouble,z0::Cdouble,μ0::Cdouble,λe::Cdouble)
     # compute the elementary integrals
@@ -76,6 +66,46 @@ function alignmentParameter(bp::beamProfile,r::Array{Cdouble,1},θ::Array{Cdoubl
     H_r,H_rθy,H_r_ph,H_rθy_ph,Arn,Aθj,Ayk,sum(H_r_ph)/sum(H_r)
 end
 
+"""
+    alignmentParameterSmoothEdge(bp::beamProfile,r::Array{Cdouble,1},θ::Array{Cdouble,1},y::Array{Cdouble,1},x0::Cdouble,y0::Cdouble,z0::Cdouble,μ0::Cdouble,Δr::Cdouble,λe::Cdouble;κ::Cdouble=5.0,Nτ::Int64=20)
+
+    Compute the volume integrales (exact for piecewise linear gain)
+
+    H_{n,j,k} = ∭ e_n(r)e_j(θ)e_k(y) f_beam(r,θ,y) e^{-∫ρ_tot(M(τ))dτ/(ρ_tot(0) λe)} rdrdθdy
+    H_{n,j,k} = ∭ e_n(r)e_j(θ)e_k(y) e^{-∫ρ_tot(M(τ))dτ/(ρ_tot(0) λe)} rdrdθdy
+
+    The arrays r, θ and y are the discretization subdivisions
+    P:(x0,y0,z0) is the point in Cartesian coordinates of the aperture analyzer
+    μ0 is the radius of the cylinder
+    λe is the attenuation length in the Beer-Lambert model
+    bp: a beam profile object that describes the deviation from the center of the target and the spatial spread of the beam
+
+    ρ_tot(r) = ρ_tot(0)/(1 + e^{(r-μ0)/Δr}), where ρ_tot(0) is the bulk total concentration and Δr the transition length of the liquid-vapor interface
+    The integral ∫ρ_tot(τ)dτ is computed over the interval [0,μ0+κΔr], where M(0)=(r,θ,y) and M(μ0+κΔr) is located at a distance μ0+κΔr from the center 
+    of the sample and in the direction MP. The integral is approximated by a quadrature with Nτ discretization nodes.
+"""
+function alignmentParameterSmoothEdge(bp::beamProfile,r::Array{Cdouble,1},θ::Array{Cdouble,1},y::Array{Cdouble,1},x0::Cdouble,y0::Cdouble,z0::Cdouble,μ0::Cdouble,Δr::Cdouble,λe::Cdouble;κ::Cdouble=5.0,Nτ::Int64=20)
+    #compute the model and elementary integrals
+    H_r,H_rθy,Arn,Aθj,Ayk = cylinder_gain_smooth_H(r,θ,y,x0,y0,z0,μ0,Δr,λe;κ=κ,Nτ=Nτ);
+    
+    # contribution of the photon beam 
+    H_rθy_ph = zeros(Cdouble,length(r),length(θ),length(y));
+    bpx      = (1.0/(2π*bp.σx*bp.σy))*exp.(-((r*sin.(θ)'.-bp.xc).^2)./(2.0*bp.σx^2));
+    for k in 1:length(y)
+        f_p = bpx*exp.(-((y[k].-bp.yc).^2)./(2.0*bp.σy^2))'
+        H_rθy_ph[:,:,k] = H_rθy[:,:,k].*f_p;
+    end
+
+    # radial model with and without beam profile
+    H_r_ph  = zeros(Cdouble,length(r));
+    for n in 1:length(r)
+        H_r_ph[n] = Arn[n]*Aθj'*H_rθy_ph[n,:,:]*Ayk
+    end
+
+    # return models and alignment parameter 
+    H_r,H_rθy,H_r_ph,H_rθy_ph,Arn,Aθj,Ayk,sum(H_r_ph)/sum(H_r)
+end
+
 
 """
     alignmentParameterSphere(bp::beamProfile,r::Array{Cdouble,1},φ::Array{Cdouble,1},θ::Array{Cdouble,1},x0::Cdouble,y0::Cdouble,z0::Cdouble,μ0::Cdouble,λe::Cdouble)
@@ -85,19 +115,20 @@ end
     H_{n,j,k} = ∭ e_n(r)e_j(φ)e_k(θ) f_beam(r,θ,y) e^{-d_P(M)/λe} r^2sin(φ)drdφdθ
     H_{n,j,k} = ∭ e_n(r)e_j(φ)e_k(θ) e^{-d_P(M)/λe} r^2sin(φ)drdφdθ
 
-    The geometry of the sample is so that Oy is the symmetry axis (along the droplet stream direction),
-    Oz is along the photon beam and Ox is the remaining axis in the orthogonal basis.
+    The geometry of the sample is so that Oz is along the droplet stream direction,
+    Ox is along the photon beam and Oy is the remaining axis in the orthogonal basis.
 
     The arrays r, φ and θ are the discretization subdivisions (r: radial distance, φ polar angle, and θ azimuthal angle).
-    The polar axis is Oy, φ is the polar angle between M and Oy, θ is taken between Oz and the project of M onto the plane xOz, and r = √(x^2+y^2+z^2)
+    The polar axis is Oz, φ is the polar angle between M and Oz, θ is taken between Ox and the project of M onto the plane xOy, and r = √(x^2+y^2+z^2)
 
     P:(x0,y0,z0) is the point in Cartesian coordinates used for the computation of the distance d_P
     μ0 is the radius of the cylinder
     λe is the attenuation length in the Beer-Lambert model
     bp: a beam profile object that describes the deviation from the center of the target and the spatial spread of the beam
+    bp is not in the same frame of reference as the distance function d_sphere_P (cylindrical frame with symmetry axis Oy, and spherical frame with polar axis Oz)
+    bp.σx: is the spread of the photon beam along Oy (horizontal)
+    bp.σy: is the spread of the photon beam along Oz (vertical)
 
-    Note: does not include the profile of the beam light, 
-    but it can be add easily if known, see alignmentParameterSphere
 """
 function alignmentParameterSphere(bp::beamProfile,r::Array{Cdouble,1},φ::Array{Cdouble,1},θ::Array{Cdouble,1},x0::Cdouble,y0::Cdouble,z0::Cdouble,μ0::Cdouble,λe::Cdouble)
     # compute the elementary integrals
@@ -114,13 +145,13 @@ function alignmentParameterSphere(bp::beamProfile,r::Array{Cdouble,1},φ::Array{
     #compute the model
     H_rφθ    = zeros(Cdouble,length(r),length(φ),length(θ));
     H_rφθ_ph = zeros(Cdouble,length(r),length(φ),length(θ));
-    y = r*cos.(φ');
-    x = r*sin.(φ');
     
     xym = r*sin.(φ')
+    zm  = r*cos.(φ')
+    f_pz = (1.0/(2π*bp.σx*bp.σy)).*exp.(-((zm.-bp.yc).^2/(2.0*bp.σy^2))) # horizontal spread
     for k in 1:length(θ)
         H_rφθ[:,:,k] = exp.(-d_sphere_P(r,φ,θ[k],x0,y0,z0,μ0)/λe)
-        local f_p = (1.0/(2π*bp.σx*bp.σy)).*exp.(-((cos(θ[k])*xym.-bp.xc).^2/(2.0*bp.σx^2))).*exp.(-((sin(θ[k])*xym.-bp.yc).^2/(2.0*bp.σy^2)))
+        local f_p = f_pz.*exp.(-((sin(θ[k])*xym.-bp.xc).^2/(2.0*bp.σx^2))) # vertical spread
         H_rφθ_ph[:,:,k] = H_rφθ[:,:,k].*f_p;
     end
 
@@ -134,6 +165,56 @@ function alignmentParameterSphere(bp::beamProfile,r::Array{Cdouble,1},φ::Array{
     H_r,H_rφθ,H_r_ph,H_rφθ_ph,Arn,Aφj,Aθk,sum(H_r_ph)/sum(H_r)
 end
 
+"""
+    alignmentParameterSphereSmoothEdge(bp::beamProfile,r::Array{Cdouble,1},φ::Array{Cdouble,1},θ::Array{Cdouble,1},x0::Cdouble,y0::Cdouble,z0::Cdouble,μ0::Cdouble,Δr::Cdouble,λe::Cdouble;κ::Cdouble=5.0,Nτ::Int64=20)
+
+    Compute the volume integrales (exact for piecewise linear gain)
+
+    H_{n,j,k} = ∭ e_n(r)e_j(φ)e_k(θ) f_beam(r,θ,y) e^{-∫ρ_tot(M(τ))dτ/(ρ_tot(0) λe)} r^2sin(φ)drdφdθ
+    H_{n,j,k} = ∭ e_n(r)e_j(φ)e_k(θ) e^{-∫ρ_tot(M(τ))dτ/(ρ_tot(0) λe)} r^2sin(φ)drdφdθ
+
+    The geometry of the sample is so that Oz is along the droplet stream direction,
+    Ox is along the photon beam and Oy is the remaining axis in the orthogonal basis.
+
+    The arrays r, φ and θ are the discretization subdivisions (r: radial distance, φ polar angle, and θ azimuthal angle).
+    The polar axis is Oz, φ is the polar angle between M and Oz, θ is taken between Ox and the project of M onto the plane xOy, and r = √(x^2+y^2+z^2)
+
+    P:(x0,y0,z0) is the point in Cartesian coordinates used for the computation of the distance d_P
+    μ0 is the radius of the cylinder
+    λe is the attenuation length in the Beer-Lambert model
+    bp: a beam profile object that describes the deviation from the center of the target and the spatial spread of the beam
+    bp is not in the same frame of reference as the distance function d_sphere_P (cylindrical frame with symmetry axis Oy, and spherical frame with polar axis Oz)
+    bp.σx: is the spread of the photon beam along Oy (horizontal)
+    bp.σy: is the spread of the photon beam along Oz (vertical)
+
+    ρ_tot(r) = ρ_tot(0)/(1 + e^{(r-μ0)/Δr}), where ρ_tot(0) is the bulk total concentration and Δr the transition length of the liquid-vapor interface
+    The integral ∫ρ_tot(τ)dτ is computed over the interval [0,μ0+κΔr], where M(0)=(r,φ,θ) and M(μ0+κΔr) is located at a distance μ0+κΔr from the center 
+    of the sample and in the direction MP. The integral is approximated by a quadrature with Nτ discretization nodes.
+
+"""
+function alignmentParameterSphereSmoothEdge(bp::beamProfile,r::Array{Cdouble,1},φ::Array{Cdouble,1},θ::Array{Cdouble,1},x0::Cdouble,y0::Cdouble,z0::Cdouble,μ0::Cdouble,Δr::Cdouble,λe::Cdouble;κ::Cdouble=5.0,Nτ::Int64=20)
+    # compute the model and elementary integrals
+    H_r,H_rφθ,Arn,Aφj,Aθk = sphere_gain_smooth_H(r,φ,θ,x0,y0,z0,μ0,Δr,λe;κ=κ,Nτ=Nτ)
+
+    # contribution of the photon beam non-uniformity
+    H_rφθ_ph = zeros(Cdouble,length(r),length(φ),length(θ));
+    xym = r*sin.(φ')
+    zm  = r*cos.(φ')
+    f_pz = (1.0/(2π*bp.σx*bp.σy)).*exp.(-((zm.-bp.yc).^2/(2.0*bp.σy^2))) # horizontal spread
+    for k in 1:length(θ)
+        local f_p = f_pz.*exp.(-((sin(θ[k])*xym.-bp.xc).^2/(2.0*bp.σx^2))) # vertical spread
+        H_rφθ_ph[:,:,k] = H_rφθ[:,:,k].*f_p;
+    end
+
+    # radial model with beam profile contribution
+    H_r_ph  = zeros(Cdouble,length(r));
+    for n in 1:length(r)
+        H_r_ph[n] = Arn[n]*Aφj'*H_rφθ_ph[n,:,:]*Aθk
+    end
+
+    # return models and alignment parameter 
+    H_r,H_rφθ,H_r_ph,H_rφθ_ph,Arn,Aφj,Aθk,sum(H_r_ph)/sum(H_r)
+end
 
 """
    noiseAndParameterEstimation(σ_χ::Array{Cdouble,1},H::Array{Cdouble,1},I_data::Array{Cdouble,1},I_bg::Array{Cdouble,1},ρ::Array{Cdouble,1})
